4c37d33d5b9cf8f4b260ef2449440f9a9d36f9fa
Qarton <mathieumarkides@gmail.com>
2020-12-07 14:53:12 +0100

    Correction nom Variable

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
-const helmet = require("helmet");
+const helmet = require('helmet');
 const path = require('path');
 require('dotenv').config();
 //Connexion à MongoDB
--- controllers/stuff.js
+++ controllers/stuff.js
@@ -3,14 +3,14 @@ const fs = require('fs');
 
 //Création d'une Sauce
 exports.createSauce = (req, res, next) => {
-  const thingObject = JSON.parse(req.body.sauce);
-  const thing = new Sauce({
-    ...thingObject,
+  const sauceObject = JSON.parse(req.body.sauce);
+  const sauce = new Sauce({
+    ...sauceObject,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
     likes: 0,
     dislikes: 0,
   });
-  thing.save()
+  sauce.save()
     .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
     .catch(error => res.status(400).json({ error }));
 };
@@ -18,17 +18,17 @@ exports.createSauce = (req, res, next) => {
 //Modification d'une Sauce
 exports.getOneSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
-    .then(thing => res.status(200).json(thing))
+    .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json({ error }));
 };
 
 exports.modifySauce = (req, res, next) => {
-  const thingObject = req.file ?
+  const sauceObject = req.file ?
     {
       ...JSON.parse(req.body.sauce),
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
-  Sauce.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Objet modifié !' }))
     .catch(error => res.status(400).json({ error }));
 };
@@ -37,8 +37,8 @@ exports.modifySauce = (req, res, next) => {
 //Suppression d'une sauce
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
-    .then(thing => {
-      const filename = thing.imageUrl.split('/images/')[1];
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
       fs.unlink(`images/${filename}`, () => {
         Sauce.deleteOne({ _id: req.params.id })
           .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
@@ -50,7 +50,7 @@ exports.deleteSauce = (req, res, next) => {
 
 exports.getAllStuff = (req, res, next) => {
   Sauce.find()
-    .then(things => res.status(200).json(things))
+    .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({ error }));
 
 };
